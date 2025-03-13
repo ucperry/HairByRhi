28b245c62e979fcfb10935e5f4d15f9b3b2d55c4
ucperry <p.wolfe@icloud.com>
2025-03-13 09:44:53 -0400

    info tags in php

--- app-form.php
+++ app-form.php
@@ -8,33 +8,28 @@ $password = "The12makeit#1";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
-
 if (mysqli_connect_error()) {
     die("Connection error: " . mysqli_connect_error());
 }
 
-
-
-//autocapture Sumbission data
+//Autocapture Form Data
 $submissionDate = date("Y-m-d H:i:s");
 $ipAddress = $_SERVER['REMOTE_ADDR'];
 $referer = $_SERVER['HTTP_REFERER'];
 
-//formcapture user input data
+//User Submitted ID Data
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 
-// Using DateTime class to handle date and time with mm/dd/yyyy format
-$rApptDate = DateTime::createFromFormat('m/d/Y', $_POST['rApptDate'])->format('Y-m-d');
-$rApptTime = DateTime::createFromFormat('h:i a', $_POST['rApptTime'])->format('H:i:s');
-
-// Client specifics regaurding the services required or curiosities to the stylists availability
+//User Submitted Appointment Data
+$rApptDate = date('Y-m-d', strtotime($_POST['rApptDate']));
+$rApptTime = date("g:i a", strtotime($_POST['rApptTime']));
 $clientMessage = $_POST['clientMessage'];
 
 
-//insert data into database
+//SQL Statement
 $sql= "INSERT INTO client_appt_request (submissionDate, 
                                         ipAddress, 
                                         referer, 
@@ -47,10 +42,10 @@ $sql= "INSERT INTO client_appt_request (submissionDate,
                                         clientMessage) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-//prepare statement
+//initialize statement
 $stmt = mysqli_stmt_init($conn);
 
-//check if statement is valid
+//prepare statement
 if (! mysqli_stmt_prepare($stmt, $sql)) {
     die(mysqli_connect_error($conn));
 }
@@ -72,7 +67,7 @@ mysqli_stmt_bind_param  ($stmt, "ssssssssss",
 mysqli_stmt_execute($stmt);
 
 //close statement
-echo "Thank you for your appointment request. We will be in touch soon.";
+echo "Thank you for your appointment request to Hair by Rhiannon, We will be in touch soon.";
 
 ?>
  
